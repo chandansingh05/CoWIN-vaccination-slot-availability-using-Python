c1e4698d27f552e790e882036247eb8b8ff2c530
chandan kumar <72074627+chandansingh05@users.noreply.github.com>
2021-05-06 18:12:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,110 @@
+import datetime
+import json
+import numpy as np
+import requests
+import pandas as pd
+import streamlit as st
+from copy import deepcopy
+
+st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
+
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def load_mapping():
+    df = pd.read_csv("district_mapping.csv")
+    return df
+
+def filter_column(df, col, value):
+    df_temp = deepcopy(df.loc[df[col] == value, :])
+    return df_temp
+
+
+mapping_df = load_mapping()
+
+mapping_dict = pd.Series(mapping_df["district id"].values,
+                         index = mapping_df["district name"].values).to_dict()
+
+rename_mapping = {
+    'date': 'Date',
+    'min_age_limit': 'Minimum Age Limit',
+    'available_capacity': 'Available Capacity',
+    'pincode': 'Pincode',
+    'name': 'Hospital Name',
+    'state_name' : 'State',
+    'district_name' : 'District',
+    'block_name': 'Block Name',
+    'fee_type' : 'Fees'
+    }
+
+st.title('CoWIN Vaccination Slot Availability')
+st.info('The CoWIN APIs are geo-fenced so sometimes you may not see an output! Please try after sometime ')
+
+# numdays = st.sidebar.slider('Select Date Range', 0, 100, 10)
+unique_districts = list(mapping_df["district name"].unique())
+unique_districts.sort()
+
+left_column_1, right_column_1 = st.beta_columns(2)
+with left_column_1:
+    numdays = st.slider('Select Date Range', 0, 100, 5)
+
+with right_column_1:
+    dist_inp = st.selectbox('Select District', unique_districts)
+
+DIST_ID = mapping_dict[dist_inp]
+
+base = datetime.datetime.today()
+date_list = [base + datetime.timedelta(days=x) for x in range(numdays)]
+date_str = [x.strftime("%d-%m-%Y") for x in date_list]
+
+final_df = None
+for INP_DATE in date_str:
+    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(DIST_ID, INP_DATE)
+    response = requests.get(URL)
+    if (response.ok) and ('centers' in json.loads(response.text)):
+        resp_json = json.loads(response.text)['centers']
+        if resp_json is not None:
+            df = pd.DataFrame(resp_json)
+            if len(df):
+                df = df.explode("sessions")
+                df['min_age_limit'] = df.sessions.apply(lambda x: x['min_age_limit'])
+                df['available_capacity'] = df.sessions.apply(lambda x: x['available_capacity'])
+                df['date'] = df.sessions.apply(lambda x: x['date'])
+                df = df[["date", "available_capacity", "min_age_limit", "pincode", "name", "state_name", "district_name", "block_name", "fee_type"]]
+                if final_df is not None:
+                    final_df = pd.concat([final_df, df])
+                else:
+                    final_df = deepcopy(df)
+            else:
+                st.error("No rows in the data Extracted from the API")
+#     else:
+#         st.error("Invalid response")
+
+if (final_df is not None) and (len(final_df)):
+    final_df.drop_duplicates(inplace=True)
+    final_df.rename(columns=rename_mapping, inplace=True)
+
+    left_column_2, center_column_2, right_column_2 = st.beta_columns(3)
+    with left_column_2:
+        valid_pincodes = list(np.unique(final_df["Pincode"].values))
+        pincode_inp = st.selectbox('Select Pincode', [""] + valid_pincodes)
+        if pincode_inp != "":
+            final_df = filter_column(final_df, "Pincode", pincode_inp)
+
+    with center_column_2:
+        valid_age = [18, 45]
+        age_inp = st.selectbox('Select Minimum Age', [""] + valid_age)
+        if age_inp != "":
+            final_df = filter_column(final_df, "Minimum Age Limit", age_inp)
+
+    with right_column_2:
+        valid_payments = ["Free", "Paid"]
+        pay_inp = st.selectbox('Select Free or Paid', [""] + valid_payments)
+        if pay_inp != "":
+            final_df = filter_column(final_df, "Fees", pay_inp)
+
+    table = deepcopy(final_df)
+    table.reset_index(inplace=True, drop=True)
+    st.table(table)
+else:
+    st.error("Unable to fetch data currently, please try after sometime")
+
+st.markdown("_- Chandan Kumar_")
